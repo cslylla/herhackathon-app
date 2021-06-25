9d02338533108d8773ab368dce421bad1938c7e7
cslylla <81328857+cslylla@users.noreply.github.com>
2021-06-25 14:03:11 +0200

    Updating team name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>The FierceFive</h1>
+        <h1>🥑 Avo-cardios 🥑</h1>
         <h2>How to match AI &amp; Inclusion</h2>
       </header>
     </div>
