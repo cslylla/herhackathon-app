e4203ce7bd94fcc51758bbad8433142df48e82a1
cslylla <81328857+cslylla@users.noreply.github.com>
2021-06-25 15:30:40 +0200

    Adding Bootstrap and google fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
+
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>🥑 Avo-cardios 🥑</h1>
         <h2>How to match AI &amp; Inclusion</h2>
+        <button type="button" className="btn btn-primary">Hello</button>
       </header>
     </div>
   );
