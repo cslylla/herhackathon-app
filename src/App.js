e2689a0f9d63cab22f4653acfa695267a583643a
cslylla <81328857+cslylla@users.noreply.github.com>
2021-06-25 17:19:57 +0200

    Adding logos to the page, updating colors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-
+import Logo1 from "./images/Deloitte.png"
+import Logo2 from "./images/HerHackathon.png"
+import Logo3 from "./images/TheHachathonCompany.png"
 
 export default function App() {
   return (
@@ -9,6 +11,13 @@ export default function App() {
         <h2>How to match AI &amp; Inclusion</h2>
         <button type="button" className="btn btn-primary">Hello</button>
       </header>
+      <section>
+        <div className="logos">
+          <div><img src={Logo1} alt="Deloitte logo" className="logo img-fluid" /></div>
+          <div><img src={Logo2} alt="HerHAckathon logo" className="logo img-fluid" /></div>
+          <div><img src={Logo3} alt="The Hackathon Company logo" className="logo img-fluid" /></div>
+        </div>
+      </section>
     </div>
   );
 }
