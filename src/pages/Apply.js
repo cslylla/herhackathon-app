0b3e73da47382e7d43cb3195576f1de2f5c2c2a7
cslylla <81328857+cslylla@users.noreply.github.com>
2021-06-27 01:42:22 +0200

    working on getting the data from the form

--- src/pages/Apply.js
+++ src/pages/Apply.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../Apply.css';
 
 export default function Apply(){
+    let [data, updateData] = useState(null);
+
+    function handleSubmit(event){
+        event.preventDefault();
+        alert("submitted");
+    }
+
+    function getData(event){
+        updateData(event.target.value);
+    }
+
     return(
     <div className="Apply">
     <div className="container">
         <h1>Application form</h1>
         <h2>Stage 1: Pre-evaluation</h2>
         <p>Complete the form below to see if you are eligible for loan.</p>
-    <form>
+    <form onSubmit={handleSubmit}>
     <div className="row">
     <div className="col-lg-2"></div>
     <div className="col-lg-5">Your monthly income in EUR</div>
     <div className="col-lg-3">
-        <input type="text" placeholder="Enter a number" autoFocus={true} autoComplete="false"/>
+        <input type="text" placeholder="Enter a number" autoFocus={true} autoComplete="false" onChange={getData}/>
     </div>
     </div>
     <div className="row">
